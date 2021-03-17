07d074a4a5a64dd529701b80ce0f0cc9d3731ad4
GitHubChrisRice <80618407+GitHubChrisRice@users.noreply.github.com>
2021-03-16 21:55:24 -0700

    Update tideconfig.py

--- tideconfig.py
+++ tideconfig.py
@@ -6,9 +6,9 @@ rh = {
 }
 
 config = {
-    "statusEmailAddress": "xxxxxxxxxx@vtext.com",
+    "statusEmailAddress": "3.14159.rice@gmail.com",
     "buyLimit": 0.0075,
-    "sellLimit": 0.01,
+    "sellLimit": 0.02,
     "movingAverageWindows": 24,    # 4 hours * 6 samples per hour
     "runMinute": [5,15,25,35,45,55],
     "coinList": ["BCH", "ETH"],
